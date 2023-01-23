6898d141364ee480bc40e599d01bf00fe04bffc5
Daniel Salgado <danielsalgado.rom@gmail.com>
2023-01-23 17:08:03 -0600

    Home Page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import { type } from "os"
-import { useState } from "react"
-import Navbar from "./scenes/navbar"
-import { SelectedPage } from "./shared/types"
+import { useState, useEffect } from "react";
+import Navbar from "./scenes/navbar";
+import Home from "@/scenes/home/index"
+import Benefits from "./scenes/benefits";
+import { SelectedPage } from "./shared/types";
 
 function App() {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
 
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+  
+  
   return (
     <div className="app bg-gray-20">
       <Navbar 
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      
+      <Home 
+        setSelectedPage={setSelectedPage}
+      />
+      <Benefits
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,15 @@
+import { SelectedPage } from "@/shared/types";
+import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
+
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Benefits = ({setSelectedPage}: Props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Benefits
